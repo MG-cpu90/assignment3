11884bb4b5eba85f9f3ef7826ccbbe84baa7e03a
Massimo Guida <massimo.guidamusic@gmail.com>
2020-02-12 01:20:46 -0500

    update repo

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,29 @@
 // Assignment Code
+// Query selectors
 var generateBtn = document.querySelector("#generate");
-var password = document.querySelector("textarea");
+var password = document.querySelector("#password");
 console.log(generateBtn);
 
-// var specialCharacters = [ "!", """", "#", "$", "%", "&", ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, ~, ", ];
-var allCharacters = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var upperAndLowercaseAlphabet = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var upperAndLowercaseAlphabetAndNumbers = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
-var upperAndLowercaseAlphabetAndSpecialCharacters = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var uppercaseAlphabetAndNumbers = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
-var uppercaseAlphabetAndSpecialCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var uppercaseAlphabetAndNumbersAndSpecialCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var lowercaseAlphabetAndNumbers = ["abcdefghijklmnopqrstuvwxyz0123456789"];
-var lowercaseAlphabetAndSpecialCharacters = ["abcdefghijklmnopqrstuvwxyz !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var lowerAlphabetAndNumbersAndSpecialCharacters = ["abcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var specialCharacters = [" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
-var randomString = [""];
+// variables for all letters, numbers, and characters)
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var randomString = "";
+var optionsVariable = "";
 console.log(specialCharacters);
 console.log(lowercaseAlphabet);
 console.log(uppercaseAlphabet);
 console.log(numbers);
-// var lowercaseLetters = lowercaseAlphabet.split(" ");
-// var uppercaseLetters = uppercaseAlphabet.split(" ");
-// var numbersString = numbers.split(" ");
-// var specialCharactersString = specialCharacters.split(" ");
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write a function that generates a random password in the "text area" box
-function writePassword(randomString) {
+function writePassword() {
+
+ // Clear the password
+ password.value = "";
 
   var lowercaseLett = confirm("Do you want your password to contain lowercase letters?");
   var uppercaseLett = confirm("Do you want your password to contain uppercase letters?");
@@ -42,7 +32,6 @@ function writePassword(randomString) {
   var passwordLength = prompt("How long do you want your password to be? Choose a password length of at least 8 characters and no more than 128 characters.");
 
 // Write another function that checks if it's between 8 and 128 characters
-// (passwordLength >= 8 || passowrdLength <= 128)
 if (passwordLength < 8 || passwordLength > 128) {
   alert("Your password is not between 8 characters and 128 characters. Please try again.");
   var passwordLength = prompt("How long do you want your password to be? Choose a password length of at least 8 characters and no more than 128 characters.");
@@ -56,50 +45,37 @@ else if (lowercaseLett === false && uppercaseLett === false && num === false && 
   var specialChar = confirm("Do you want your password to contain special characters?");
 }
 
+if (lowercaseLett) {
+  optionsVariable += lowercaseAlphabet;
+}
+
+if (uppercaseLett) {
+  optionsVariable += uppercaseAlphabet;
+}
+
+if (num) {
+  optionsVariable += numbers;
+}
+
+if (specialChar) {
+  optionsVariable += specialCharacters;
+}
+
   for (var i = 0; i < passwordLength; i++) {
 
-    // n1 = specialCharacters.length; n2 = lowercaseAlphabet.length; n3 = uppercaseAlphabet.length; n4 = numbers.length;
-
-    if (lowercaseLett === true || uppercaseLett === true || num === true || specialChar === true) {
-      randomString += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
+    // Generates a random password based on the criteria that have been selected
+    // Continues to select a random character value from the string until it is the desired length
+      randomString += optionsVariable.charAt(Math.floor(Math.random() * optionsVariable.length));
+      console.log(randomString);
          }
-         return randomString;
-      }
-  "textarea".insertAdjacentText(randomString);
-}
+         console.log(password);
+  password.value = randomString;
+
+  }
 
 // Log the function
 console.log(writePassword);
 
 // Call the function
-// writePassword(randomString);
-
-// var password = document.createTextNode(writePassword);
-// var addTextHere = document.getElementById("addHere");
-
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
-
-// passwordText.value = password;
-
-
-// to generate a random number between 1 and 10
-// Math.floor((Math.random() * 9) + 1);
-// console.log(Math.floor());
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-//  THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-//  THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-//  THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-//  THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-//  THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-//  THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-//  THEN the password is either displayed in an alert or written to the page
+// writePassword();
+eraseText();
